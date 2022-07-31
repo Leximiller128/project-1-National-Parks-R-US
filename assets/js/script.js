b8e40dfe0ea09a184479e9a8981e243b85d52913
Garret Nelson <garretnelson@Garrets-MacBook-Pro.local>
2022-07-31 14:16:07 -0600

    apikeyweather

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,23 @@ fetch(apiParks).then(function (response) {
     return response.json();
 })
 
+let weather = {
+    apiKey: "e606684467a0eb77dafd42310f90ff21",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=metric&appid=" +
+            this.apiKey
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No weather found.");
+                    throw new Error("No weather found.");
+                }
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    },
 
 
-
-    
