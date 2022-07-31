2519f471564b422ea43adc917fd33b5fb0123e47
Leximiller128 <leximiller128@gmail.com>
2022-07-31 14:02:00 -0600

    testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,13 @@
-
 //figure out the state park api and implement it
-
 
 //use open weather api to get the weather and use it to display the weather
 
-
-var apiParks = "https://developer.nps.gov/api/v1/parks?stateCode=" + stateName + "&api_key=MVj5HVGtvVizx4wzcJ24hUmEImkvRT5DF2t8dyk1";
+var apiParks =
+  "https://developer.nps.gov/api/v1/parks?stateCode=" +
+  stateName +
+  "&api_key=MVj5HVGtvVizx4wzcJ24hUmEImkvRT5DF2t8dyk1";
 fetch(apiParks).then(function (response) {
-    return response.json();
-})
+  return response.json();
+});
 
-
-
-
-    
+//this is a test
