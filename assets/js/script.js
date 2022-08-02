f3b933f691621fd0b7acfceef9cd59bb909f1c69
Leximiller128 <leximiller128@gmail.com>
2022-08-01 20:28:23 -0600

    added console log for parks api

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,15 @@
 //use open weather api to get the weather and use it to display the weather
 //var stateName =
 var apiParks =
-  "https://developer.nps.gov/api/v1/parks?stateCode=" +
+  "https://developer.nps.gov/api/v1/parks?q=${stateName}&stateCode=" +
   stateName +
   "&api_key=MVj5HVGtvVizx4wzcJ24hUmEImkvRT5DF2t8dyk1";
-fetch(apiParks).then(function (response) {
-  return response.json();
-})
+fetch(apiParks)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    console.log(stateName);
+  });
 // use the entered state name to get the state park api to display the state parks in the state
-
-
-
-
-
-
-
