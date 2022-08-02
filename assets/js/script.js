fbeaf23168610c923be1a5fd2478c87ed9bbdf47
Leximiller128 <leximiller128@gmail.com>
2022-08-01 18:29:35 -0600

    fixed weather and parks API, added id for stateName

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,33 @@
 //figure out the state park api and implement it
 
 //use open weather api to get the weather and use it to display the weather
-
+//var stateName =
 var apiParks =
   "https://developer.nps.gov/api/v1/parks?stateCode=" +
   stateName +
   "&api_key=MVj5HVGtvVizx4wzcJ24hUmEImkvRT5DF2t8dyk1";
 fetch(apiParks).then(function (response) {
-    return response.json();
-})
-
-let weather = {
-    apiKey: "e606684467a0eb77dafd42310f90ff21",
-    fetchWeather: function (city) {
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=" +
-            city +
-            "&units=metric&appid=" +
-            this.apiKey
-        )
-            .then((response) => {
-                if (!response.ok) {
-                    alert("No weather found.");
-                    throw new Error("No weather found.");
-                }
-                return response.json();
-            })
-            .then((data) => this.displayWeather(data));
-    },
   return response.json();
 });
 
-//this is a test
-//this is another test
+let weather = {
+  apiKey: "e606684467a0eb77dafd42310f90ff21",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+    //}
+    return response.json();
+  },
+};
