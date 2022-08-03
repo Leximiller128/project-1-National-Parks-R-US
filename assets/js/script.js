de6024279f93b769eb7fc5e2e76e0fc86e641bb9
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-08-03 08:20:33 -0600

    added function to pull random fun fact from array of fun facts

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,3 +79,13 @@ function modalContent() {
   //add random array to modal in html
   document.querySelector(".box").innerHTML = result;
 }
+//pull random item from array
+function getRandomItem(arr) {
+  // get random index value
+  const randomIndex = Math.floor(Math.random() * arr.length);
+
+  // get random item
+  const item = arr[randomIndex];
+
+  return item;
+}
